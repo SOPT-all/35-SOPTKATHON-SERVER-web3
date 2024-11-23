65ea2ed0622e1e71b1f4cb2db8b478f07c1e7e78
paragon0107 <smg0725@naver.com>
2024-11-24 06:56:24 +0900

    w

--- src/main/java/sopt/web3/demo/common/config/WebConfig.java
+++ src/main/java/sopt/web3/demo/common/config/WebConfig.java
@@ -14,10 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "*"
-                )
-                .allowedMethods("*");
-
+                        "https://35-sopkathon-web-web3.vercel.app/",
+                        "http://localhost:5173"
+                        )
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 
 }
