6a522fd05bef74ea776381616d996409834d4128
학림 <63008138+hfjxjjd123@users.noreply.github.com>
2024-11-23 20:44:46 +0900

    Add files via upload
    init

---
+++ demo/src/test/java/sopt/web3/demo/Web3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.web3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
