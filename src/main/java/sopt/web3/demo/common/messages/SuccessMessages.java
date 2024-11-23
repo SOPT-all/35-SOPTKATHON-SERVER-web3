f7edc3e6f754fa684e6760e28833d348f2ceba5a
paragon0107 <smg0725@naver.com>
2024-11-24 04:30:48 +0900

    w

--- src/main/java/sopt/web3/demo/common/messages/SuccessMessages.java
+++ src/main/java/sopt/web3/demo/common/messages/SuccessMessages.java
@@ -1,7 +1,7 @@
 package sopt.web3.demo.common.messages;
 
 public enum SuccessMessages {
-    GET_LEVEL("성공"),
+    GET_LEVEL("`     .-=-= b 1성공"),
     GET_TODOLISTS("히스토리를 성공적으로 받아왔습니다."),
     GET_ALL_PERCENTAGE("성공률 관련 데이터를 불러왔습니다.");
 
