2727c73e52e7d7136771f99cfbe549f2712d5cd2
paragon0107 <smg0725@naver.com>
2024-11-24 02:59:44 +0900

    w

--- src/main/java/sopt/web3/demo/common/dto/ErrorResponse.java
+++ src/main/java/sopt/web3/demo/common/dto/ErrorResponse.java
@@ -1,14 +1,11 @@
 package sopt.web3.demo.common.dto;
 
-import sopt.market.common.util.NullValidator;
-
 public record ErrorResponse(
 	boolean success,
 	String message
 ) implements BaseResponse {
 
 	public static ErrorResponse of(final String message) {
-		NullValidator.valid(message);
 		return new ErrorResponse(false, message);
 	}
 }
