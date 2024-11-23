08810df3bc4c22ab7697735f2c30ce6dd8288677
hfjxjjd123 <licbusiness21@gmail.com>
2024-11-24 05:12:06 +0900

    as

---
+++ src/main/java/sopt/web3/demo/common/config/WebConfig.java
@@ -0,0 +1,26 @@
+package sopt.web3.demo.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://35-sopkathon-web-web3.vercel.app/",
+                        "https://localhost:5173"
+                        )
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
