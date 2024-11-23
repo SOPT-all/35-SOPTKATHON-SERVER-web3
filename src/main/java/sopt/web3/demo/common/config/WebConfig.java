daf1d44a43a68b6d6b49dc5c7d382438e64a2755
paragon0107 <smg0725@naver.com>
2024-11-24 07:08:20 +0900

    w

--- src/main/java/sopt/web3/demo/common/config/WebConfig.java
+++ src/main/java/sopt/web3/demo/common/config/WebConfig.java
@@ -1,10 +1,18 @@
 package sopt.web3.demo.common.config;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
 
+import java.io.IOError;
+import java.io.IOException;
 import java.util.List;
 
 @Configuration
@@ -23,4 +31,25 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600);
     }
 
+    @Component
+    public static class CorsFilter implements Filter {
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+            final HttpServletRequest req = (HttpServletRequest) request;
+            final HttpServletResponse res = (HttpServletResponse) response;
+
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+            res.setHeader("Access-Control-Allow-Methods", "*");
+            res.setHeader("Access-Control-Max-Age", "3600");
+            res.setHeader("Access-Control-Allow-Headers", "*");
+
+            if("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+                res.setStatus(HttpServletResponse.SC_OK);
+            } else {
+                chain.doFilter(req, res);
+            }
+        }
+    }
 }
